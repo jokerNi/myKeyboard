6be39afd7e6a8710540219fab3a323606b772cbe
colin <hitwhylz@163.com>
2014-06-07 23:14:18 +0800

    myKeyBoard
    myKeyBoard

--- myKeyboard/ViewController.h
+++ myKeyboard/ViewController.h
@@ -10,4 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *myTextView;
+
+@property(nonatomic, retain) UIToolbar *keyboardToolbar;
+
+
 @end
--- myKeyboard/ViewController.m
+++ myKeyboard/ViewController.m
@@ -13,11 +13,61 @@
 @end
 
 @implementation ViewController
+@synthesize keyboardToolbar;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    if (self.keyboardToolbar == nil)
+    {
+        self.keyboardToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 38.0f)];
+        self.keyboardToolbar.barStyle = UIBarStyleBlackTranslucent;
+        
+        UIBarButtonItem *previousBarItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"前进", @"")
+                                                                            style:UIBarButtonItemStyleBordered
+                                                                           target:self
+                                                                           action:@selector(previousField:)];
+        
+        UIBarButtonItem *nextBarItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"后退", @"")
+                                                                        style:UIBarButtonItemStyleBordered
+                                                                       target:self
+                                                                       action:@selector(nextField:)];
+        
+        UIBarButtonItem *spaceBarItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                                      target:nil
+                                                                                      action:nil];
+        
+        UIBarButtonItem *doneBarItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"隐藏", @"")
+                                                                        style:UIBarButtonItemStyleDone
+                                                                       target:self
+                                                                       action:@selector(resignKeyboard:)];
+        
+        [self.keyboardToolbar setItems:[NSArray arrayWithObjects:previousBarItem, nextBarItem, spaceBarItem, doneBarItem, nil]];
+    }
+    
+    self.myTextView.inputAccessoryView = self.keyboardToolbar;
+
+}
+
+
+#pragma mark - your code
+- (void)resignKeyboard:(id)sender
+{
+    [self.myTextView resignFirstResponder];
+}
+
+- (void)previousField:(id)sender
+{
+
+}
+
+- (void)nextField:(id)sender
+{
+
 }
 
 - (void)didReceiveMemoryWarning
